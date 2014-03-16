bc921f8f4a687d34878066b39358fe3fa58a2428
Haechan Lee <soport55@thefermata.net>
2014-03-16 12:12:21 +0900

    전체재생버튼 수정
    벅스쪽에서 수정한 전체재생 부분을 반영함

--- changeListenFunction.js
+++ changeListenFunction.js
@@ -21,6 +21,63 @@ var scriptToRun = function(){
 			alert('재생목록에 곡이 추가되었습니다.');
 		}
 	}
+	bugs.music.trackList.prototype.listenAll = function (btn) {
+		var listenForce = arguments[1];if(listenForce==null)listenForce=true;
+		var recommendList = $("li.listRow div.recommend");
+		var aList = $("input:hidden[name=_isStream]:not(:disabled)", this.box);
+
+		if(aList.length == 0) {
+			bugs.ui.showAlert("재생 가능한 곡이 없습니다.", {css : "layerLoginMsg"});
+			return;
+		}
+
+		$("#_checkAll"+this.name).attr("checked",true).parent().parent().addClass("click");
+		$(this.trackChecked).each(function() {
+			var _this  = this;
+			aList.each(function() {
+				if(this.value == _this.val()) _this.checked();
+			});
+		});
+
+		//this.cbox.checked();
+		var chks = $("ul li input:hidden[name=_isStream]", this.box).not("input:disabled");
+
+		if(typeof arguments[2] != "undefined" && arguments[2] == "no"){
+			//추천곡 전체 재생 제외
+			if(recommendList.length > 0){
+				var recommend = $("li.listRow");
+				var recommend_chk = $("li.listRow div.check");
+
+				for(var i=0;i < recommendList.length;i++){
+					recommend.eq(i).removeClass("rowSelect");
+					recommend_chk.eq(i).removeClass("click");
+				}
+
+				chks = chks.splice(recommendList.length , chks.length);
+			}
+		}
+		if(typeof chks == "undefined") return;
+		if(typeof chks == "string") {
+		} else if(typeof chks == "object") {
+			var trackIdStr = "";
+			if(chks.length == 0) {
+				bugs.ui.showAlert("음악을 선택하여 주십시오.", {css : "layerAdultNotice"});
+				return;
+			}
+			for(var i=0;i < chks.length;i++) {
+				trackIdStr += $(chks[i]).val();
+				if(i < chks.length - 1) trackIdStr +=",";
+			}
+			chks = trackIdStr;
+		}
+
+		if(!bugs.music.flashInstalled) {
+			bugs.music.flashPopup.show();
+			return;
+		}
+
+		bugs.music.listen(chks, listenForce);
+	} 
 };
 
 var scriptMadeUp = '(' + scriptToRun + ')();';
